0b9bf9e616c136cd489347f6dc96f7dab5c42958
Ilyes Soumar Djouma <ilyes.soumar@free.fr>
2024-10-15 22:50:52 +0200

    main page done

---
+++ src/components/Screen/Screen.jsx
@@ -0,0 +1,24 @@
+
+function Screen() {
+  return (
+    <div className="screen-container">
+      <div className="screen-title">Quizz'App</div>
+      <div className="screen-center">
+        <div className="screen-slogan">
+          Don&apos;t waste your time, <br />
+          learn, laugh and battle <br />
+          your friends to the top !
+        </div>
+        <div className="screen-modes">
+          <div className="screen-mode-selector">Solo Battle Mode</div>
+          <div className="screen-mode-selector">Multi Battle Mode</div>
+          <div className="screen-mode-selector">Leaderboards</div>
+          <div className="screen-mode-selector">Options</div>
+        </div>
+      </div>
+      <div className="screen-footer">Ver. 1.0.1</div>
+    </div>
+  )
+}
+
+export default Screen
